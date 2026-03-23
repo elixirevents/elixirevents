bb2a215f3e2272d768e30f64456cb89b1537acbc
Rui Freitas <rodloboz@hey.com>
2026-03-23 19:55:29 +0000

    Filter events by type (#7)

--- lib/elixir_events_web/live/event_live/index.ex
+++ lib/elixir_events_web/live/event_live/index.ex
@@ -4,21 +4,30 @@ defmodule ElixirEventsWeb.EventLive.Index do
   alias ElixirEvents.Events
 
   @valid_filters ~w(all upcoming past)
+  @valid_kinds ~w(conference meetup retreat hackathon summit workshop)
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, show_kind_filter: false)}
   end
 
   @impl true
   def handle_params(params, _uri, socket) do
     filter = if params["filter"] in @valid_filters, do: params["filter"], else: "all"
 
+    kinds =
+      case params["kinds"] do
+        nil -> []
+        str -> str |> String.split(",") |> Enum.filter(&(&1 in @valid_kinds))
+      end
+
+    query_opts = [preload: [:event_series, :cfps], kinds: if(kinds == [], do: nil, else: kinds)]
+
     events =
       case filter do
-        "upcoming" -> Events.list_upcoming_events(preload: [:event_series, :cfps])
-        "past" -> Events.list_past_events(preload: [:event_series, :cfps])
-        _ -> Events.list_events(preload: [:event_series, :cfps])
+        "upcoming" -> Events.list_upcoming_events(query_opts)
+        "past" -> Events.list_past_events(query_opts)
+        _ -> Events.list_events(query_opts)
       end
 
     {:noreply,
@@ -26,6 +35,57 @@ defmodule ElixirEventsWeb.EventLive.Index do
      |> assign(:page_title, "Events")
      |> assign(:events, events)
      |> assign(:current_filter, filter)
+     |> assign(:selected_kinds, kinds)
      |> assign(:event_count, length(events))}
+  end
+
+  @impl true
+  def handle_event("toggle-kind-filter", _params, socket) do
+    {:noreply, assign(socket, show_kind_filter: !socket.assigns.show_kind_filter)}
+  end
+
+  def handle_event("close-kind-filter", _params, socket) do
+    {:noreply, assign(socket, show_kind_filter: false)}
+  end
+
+  def handle_event("toggle-kind", %{"kind" => kind}, socket) when kind in @valid_kinds do
+    current = socket.assigns.selected_kinds
+
+    new_kinds =
+      if kind in current,
+        do: List.delete(current, kind),
+        else: current ++ [kind]
+
+    params = build_filter_params(socket.assigns.current_filter, new_kinds)
+    {:noreply, push_patch(socket, to: ~p"/events?#{params}")}
+  end
+
+  def handle_event("clear-kinds", _params, socket) do
+    params = build_filter_params(socket.assigns.current_filter, [])
+    {:noreply, push_patch(socket, to: ~p"/events?#{params}")}
+  end
+
+  def kind_options do
+    [
+      {"conference", "Conferences"},
+      {"meetup", "Meetups"},
+      {"retreat", "Retreats"},
+      {"hackathon", "Hackathons"},
+      {"summit", "Summits"},
+      {"workshop", "Workshops"}
+    ]
+  end
+
+  def kind_dot_color("conference"), do: "bg-primary"
+  def kind_dot_color("meetup"), do: "bg-secondary"
+  def kind_dot_color("retreat"), do: "bg-accent"
+  def kind_dot_color("hackathon"), do: "bg-warning"
+  def kind_dot_color("summit"), do: "bg-info"
+  def kind_dot_color("workshop"), do: "bg-success"
+  def kind_dot_color(_), do: "bg-base-content/40"
+
+  def build_filter_params(filter, kinds) do
+    params = if filter == "all", do: %{}, else: %{filter: filter}
+    if kinds == [], do: params, else: Map.put(params, :kinds, Enum.join(kinds, ","))
   end
 end
