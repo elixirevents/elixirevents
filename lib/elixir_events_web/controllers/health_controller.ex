4f0b96a2ce300b249a1cdde4f4f0dbef9d8dd1ae
Rui Freitas <rodloboz@hey.com>
2026-04-13 11:43:38 +0100

    Add Kamal deployment config, replace Fly.io + Supabase setup
    - Add config/deploy.yml for Kamal 2 targeting Hetzner ARM VPS
    - Add /up healthcheck endpoint for Kamal proxy
    - Remove Supabase SSL cert logic from runtime.exs
    - Remove Fly.io-specific IPv6/DNS clustering config
    - Run migrations before server start in Dockerfile CMD

---
+++ lib/elixir_events_web/controllers/health_controller.ex
@@ -0,0 +1,9 @@
+defmodule ElixirEventsWeb.HealthController do
+  use ElixirEventsWeb, :controller
+
+  def index(conn, _params) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "ok")
+  end
+end
